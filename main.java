e4fbbc1f87608eda9894a1273fa0c5b92e58b613
adamaxy <138890873+adamaxy@users.noreply.github.com>
2023-07-21 14:09:04 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,21 @@
 public class main {
     public static void main(String[] args) {
-     var a = 2 <= 2 && !true;   // false
-     var b = !false && 3 > 2;  // true
-     var c = !(!true || false );   // true
-     var d = 6 > 6 ^ !(true && true);  //false
-     System.out.println(a);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-}}
+        // [A]
+        boolean a_result = 2 <= 2 && !true;
+        System.out.println("[A] Result: " + a_result);
+
+        // [B]
+        boolean b_result = !false && 3 > 2;
+        System.out.println("[B] Result: " + b_result);
+
+        // [C]
+        boolean t = false;
+        boolean f = true;
+        boolean c_result = !(t || f);
+        System.out.println("[C] Result: " + c_result);
+
+        // [D]
+        boolean d_result = 6 > 6 ^ !(true && true);
+        System.out.println("[D] Result: " + d_result);
+    }
+}
